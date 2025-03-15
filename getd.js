6952f2f7793c1d8d6eb1fa26271e265428999705
ramdato <nawaf_alnagar@yahoo.com>
2025-03-15 23:02:48 +0300

    Add files via upload

---
+++ getd.js
@@ -0,0 +1,82 @@
+
+    
+        const sickLeaves = [
+            {
+                serviceCode: "PSL1",
+                patientId: "101",
+                patientName: "ياسين نجيب احمد",
+                sickLeaveDate: "2025-01-03",
+                from: "2025-01-03",
+                to: "2025-01-03",
+                duration: "1",
+                doctorName: "ساره فخرى ابراهيم عياط",
+                jobTitle: "طبيب عام"
+            },
+            {
+                serviceCode: "psl",
+                patientId: "id",
+                patientName: "name",
+                sickLeaveDate: "2025-01-03",
+                from: "2025-01-03",
+                to: "2025-01-03",
+                duration: "1",
+                doctorName: "نواف النجار",
+                jobTitle: "طبيب عام"
+            },
+            {
+                serviceCode: "PSL6",
+                patientId: "106",
+                patientName:"سكليف",
+                sickLeaveDate: "2025-01-03",
+                from: "2025-01-03",
+                to: "2025-01-03",
+                duration: "1",
+                doctorName:"سكليف",
+                jobTitle: "طبيب عام"
+            },
+            {
+                serviceCode: "PSL2",
+                patientId: "102",
+                patientName: "هاله نواف القحطاني",
+                sickLeaveDate: "2025-05-03",
+                from: "2025-01-03",
+                to: "2025-01-03",
+                duration: "1",
+                doctorName: "ساره فخرى ابراهيم عياط",
+                jobTitle: "طبيب عام"
+            },
+            
+            {
+                serviceCode: "PSL3",
+                patientId: "103",
+                patientName: "نواف عبده احمد",
+                sickLeaveDate: "2025-05-03",
+                from: "2025-09-03",
+                to: "2025-01-03",
+                duration: "1",
+                doctorName: "ساره فخرى ابراهيم عياط",
+                jobTitle: "طبيب عام"
+            },
+            {
+                serviceCode: "PSL5",
+                patientId: "105",
+                patientName: "افنان محمد سعيد",
+                sickLeaveDate: "2025-12-03",
+                from: "2025-09-03",
+                to: "2025-01-03",
+                duration: "1",
+                doctorName: "ساره فخرى ابراهيم عياط",
+                jobTitle: "طبيب عام"
+            },
+              {
+                serviceCode: "PSL4",
+                patientId: "104",
+                patientName: "بكيل سيف عقلان",
+                sickLeaveDate: "2025-02-07",
+                from: "2025-02-07",
+                to: "2025-02-07",
+                duration: "1",
+                doctorName: "هاله نواف القحطاني",
+                jobTitle: "طبيب عام"
+            }
+        ];
